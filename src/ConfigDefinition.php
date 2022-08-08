3173934cd62c3ec8552dd2fd6574d0f70268340b
Výborný Adam <mail@adamvyborny.cz>
2022-08-08 08:51:38 +0200

    Changed namespace, package name, updated git ignore, added composer.lock, updated credentials and other minor init updates

--- src/ConfigDefinition.php
+++ src/ConfigDefinition.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace MyComponent;
+namespace NoCodeDbtTransformation;
 
 use Keboola\Component\Config\BaseConfigDefinition;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
