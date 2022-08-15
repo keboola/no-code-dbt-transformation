6af9e12384de577c77fdd3d55ace7a48ce021d4f
Výborný Adam <mail@adamvyborny.cz>
2022-08-15 12:28:38 +0200

    Add test

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -3,3 +3,21 @@
 declare(strict_types=1);
 
 require __DIR__ . '/../../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createUnsafeMutable(__DIR__ . '/../../');
+$dotenv->safeLoad();
+
+$environments = [
+    'SNOWFLAKE_HOST',
+    'SNOWFLAKE_WAREHOUSE',
+    'SNOWFLAKE_DATABASE',
+    'SNOWFLAKE_SCHEMA',
+    'SNOWFLAKE_USER',
+    'SNOWFLAKE_PASSWORD',
+];
+
+foreach ($environments as $environment) {
+    if (empty(getenv($environment))) {
+        throw new RuntimeException(sprintf('Missing environment "%s".', $environment));
+    }
+}
