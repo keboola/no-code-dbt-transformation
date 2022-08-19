5b0fa18b53c82f2d813a3e56ca2d64b8af1e595d
Výborný Adam <mail@adamvyborny.cz>
2022-08-19 15:56:46 +0200

    Sync action now gets credentials from configuration

---
+++ src/ActionConfigDefinition.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NoCodeDbtTransformation;
+
+use Keboola\Component\Config\BaseConfigDefinition;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+
+class ActionConfigDefinition extends BaseConfigDefinition
+{
+    protected function getParametersDefinition(): ArrayNodeDefinition
+    {
+        $parametersNode = parent::getParametersDefinition();
+        // @formatter:off
+        /** @noinspection NullPointerExceptionInspection */
+        $parametersNode
+            ->isRequired()
+            ->children()
+                ->arrayNode('models')
+                    ->isRequired()
+                    ->cannotBeEmpty()
+                    ->prototype('scalar')
+                        ->cannotBeEmpty()
+                ->end()
+            ->end()
+                ->arrayNode('authorization')
+                    ->children()
+                        ->scalarNode('host')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('warehouse')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('database')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('schema')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('user')->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode('#password')->isRequired()->cannotBeEmpty()->end()
+                    ->end()
+                ->end()
+            ->end();
+
+        // @formatter:on
+        return $parametersNode;
+    }
+}
