5dc063fbbda338dcdf02e59424710799a08a2d53
Výborný Adam <mail@adamvyborny.cz>
2022-09-22 11:35:11 +0200

    Remove forgotten use

--- src/Component.php
+++ src/Component.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace NoCodeDbtTransformation;
 
-use JetBrains\PhpStorm\ArrayShape;
 use Keboola\Component\BaseComponent;
 use Keboola\SnowflakeDbAdapter\Connection;
 use Keboola\StorageApi\Client;
